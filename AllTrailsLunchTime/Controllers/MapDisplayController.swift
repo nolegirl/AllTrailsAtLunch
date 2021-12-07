d230fa297d3c00fb84554c77519f787f06909bca
mechellesieglitz <mechelle.sieglitz@gmail.com>
2021-12-06 21:57:35 -0500

    Set up map and zooms into users location

---
+++ AllTrailsLunchTime/Controllers/MapDisplayController.swift
@@ -0,0 +1,83 @@
+//
+//  MapDisplayController.swift
+//  AllTrailsLunchTime
+//
+//  Created by Mechelle Sieglitz on 12/6/21.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapDisplayController: UIViewController, CLLocationManagerDelegate {
+    
+    //MARK: - Properties
+    var locationManager:CLLocationManager!
+    let mapView = MKMapView()
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let leftMargin:CGFloat = 10
+        let topMargin:CGFloat = 60
+        let mapWidth:CGFloat = view.frame.size.width-20
+        let mapHeight:CGFloat = 300
+
+        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+
+        view.addSubview(mapView)
+        determineCurrentLocation()
+    }
+    
+    //MARK: - Actions
+    func determineCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+        }
+    }
+    
+
+    //MARK: - Map
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+            
+            // Call stopUpdatingLocation() to stop listening for location updates,
+            // other wise this function will be called every time when user location changes.
+            
+           // manager.stopUpdatingLocation()
+            
+            print("user latitude = \(userLocation.coordinate.latitude)")
+            print("user longitude = \(userLocation.coordinate.longitude)")
+    }
+        
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+            print("DEBUG: Error \(error)")
+    }
+}
